9a73a830a973e67900d72ee9731e2d9360ce329a
José Cleiton Correia <jose.correia@cubos.io>
2022-09-28 10:38:21 +0000

    Add saveError

--- src/etl.ts
+++ src/etl.ts
@@ -6,26 +6,27 @@ import loadDiagnostics from './loaders/loadDiagnostics';
 import loadSus from './loaders/loadSus';
 import loadProcedures from './loaders/loadProcedures';
 import loadHealthOrganization from './loaders/loadHealthOrganization';
+import { writeFileSync } from 'fs';
 
 async function execute() {
   const ds = await dataSource.initialize();
-  // const [cities, cids, procedures] = await ds.manager.transaction(async (tx) =>
-  //   Promise.all([loadCities(tx), loadDiagnostics(tx), loadProcedures(tx)]),
-  // );
+  const [cities, cids, procedures] = await ds.manager.transaction(async (tx) =>
+    Promise.all([loadCities(tx), loadDiagnostics(tx), loadProcedures(tx)]),
+  );
 
-  // const healthOrganizations = await loadHealthOrganization(ds.manager);
+  const healthOrganizations = await loadHealthOrganization(ds.manager);
 
-  // console.log({
-  //   cities: cities.length,
-  //   cids: cids.length,
-  //   procedures: procedures.length,
-  //   healthOrganizations: healthOrganizations.length,
-  // });
+  console.log({
+    cities: cities.length,
+    cids: cids.length,
+    procedures: procedures.length,
+    healthOrganizations: healthOrganizations.length,
+  });
 
   const hospitalizations = await loadSus(ds.manager, {
-    cities: [],
-    cids: [],
-    procedures: [],
+    cities,
+    cids,
+    procedures,
   });
 
   console.log({
@@ -33,4 +34,9 @@ async function execute() {
   });
 }
 
-execute().catch(console.error);
+function saveError(e) {
+  writeFileSync('error.log', JSON.stringify(e));
+  throw e;
+}
+
+execute().catch(saveError);
