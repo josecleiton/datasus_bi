3a6e526b6798113715772c496b6486a4ea614483
José Cleiton Correia <jose.correia@cubos.io>
2022-09-30 21:35:21 -0300

    More readable output on report

--- src/report.ts
+++ src/report.ts
@@ -1,18 +1,23 @@
 import { readdirSync, writeFileSync } from 'fs';
+import { chunk } from 'lodash';
 import { join } from 'path';
 import loadCsvAndExec from './helpers/loadCsvAndExec';
 
 async function execute() {
   const path = join(__dirname, '..', 'datasets', 'sus');
 
-  let result = '';
+  let result = 'mês\t-\tqtd\n';
   for (const file of readdirSync(path)) {
     const fullpath = join(path, file);
     const registers = await loadCsvAndExec<{ N_AIH: string }, string>({
       path: fullpath,
       exec: async (value) => value.N_AIH,
     });
-    result += `${file} ${registers.length}\n`;
+    const monthYear = chunk(file.replace(/\D/g, '').split(''), 2)
+      .map((x) => x.join(''))
+      .reverse()
+      .join('/');
+    result += `${monthYear}\t-\t${registers.length}\n`;
   }
 
   writeFileSync(join(__dirname, '..', 'report.log'), result);
